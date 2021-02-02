524141cacf2622dcb70fec839043865de4ee2332
faisal <faisal25march99@gmail.com>
2021-02-02 17:30:24 +0530

    DQL Select and Alias

--- HandsOnExercise.sql
+++ HandsOnExercise.sql
@@ -6,5 +6,5 @@ DML Insert Command - Hands on Exercise
 insert into Customer(First_Name, Last_Name, Username, Password, Mobile_Number, Date_of_Birth) Values ('Niki', 'Tamboli', 'Nikki123', 'ben23', 8877445671, To_Date('12-11-97','DD-MM-YY'));
 insert into Customer(First_Name, Last_Name, Username, Password, Mobile_Number, Date_of_Birth) Values ('Vikas', 'Gupta', 'Vikas@675', 'vikkifacebook', 8877445672, To_Date('19-01-87','DD-MM-YY'));
 
-select * from Customer ;
+select first_name as FirstName, Password as Password, Mobile_number as PhoneNumber from Customer ;
 
