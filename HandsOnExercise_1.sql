5b5a49fe5db885483b2474075c116ac56003009d
faisal <faisal25march99@gmail.com>
2021-02-02 12:25:24 +0530

    Done with Hands on Exercise One [ Creating Table Movie and suitable Data type ]

---
+++ HandsOnExercise_1.sql
@@ -0,0 +1,13 @@
+/*
+CREATE Command - Hands on Exercise
+*/
+
+CREATE TABLE Movie (
+movie_name VARCHAR2(255),
+movie_desc VARCHAR2(1000),
+release_date DATE,
+movie_duration NUMBER,
+cover_photo_url BLOB,
+trailer_url VARCHAR2(500),
+languahe VARCHAR2(20)
+);
--- short_query_to_find_fkey.sql
+++
@@ -1,7 +0,0 @@
-/*
-use sp_fkeys to query foreign keys for a given table:
-
-EXEC sp_fkeys 'table_name','schema_name'
-
-*/
-EXEC sp_fkeys 'tbl_events', 'jocata_star' ;
