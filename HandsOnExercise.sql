c9fb42dc83c6654adebba73da2096abd45715bbc
faisal <faisal25march99@gmail.com>
2021-02-03 11:21:15 +0530

    ADD location column

--- HandsOnExercise.sql
+++ HandsOnExercise.sql
@@ -13,3 +13,19 @@ TICKET_PRICE	 - 	NUMBER(3,0)
 */
 
 
+
+
+-- ADD Location Col
+
+ALTER TABLE MOVIE_THEATRE
+ADD location varchar2(255) ;
+
+/*
+Column	Null?	Type
+THEATRE_NAME	 - 	VARCHAR2(20)
+NO_OF_SEATS	 - 	NUMBER(3,0)
+TICKET_PRICE	 - 	NUMBER(3,0)
+LOCATION	 - 	VARCHAR2(255)
+*/
+
+
