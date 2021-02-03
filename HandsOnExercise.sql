90abda1b0da6f6a478a90eec13c31c60d4b1efb3
faisal <faisal25march99@gmail.com>
2021-02-03 11:52:20 +0530

    Delete DML Command V.IMP as per the interview

--- HandsOnExercise.sql
+++ HandsOnExercise.sql
@@ -1,13 +1,35 @@
--- Delete a column name location from movie_theatre
+/*
+Removal of record from the table
 
-ALTER TABLE MOVIE_THEATRE
-DROP COLUMN LOCATION;
+Note* If you don't use where clause with DELETE statement you may 
+accidentially delte all the records.
 
-DESC MOVIE_THEATRE
+*/
+-- Delete record from the Customers table where customer name is Vikas
+
+Select * from Customer;
+/*
+Before Delete Command
+
+FIRST_NAME	LAST_NAME	USERNAME	PASSWORD	MOBILE_NUMBER	DATE_OF_BIRTH
+Niki	Tamboli	Nikki123	ben23	8877445671	12-NOV-97
+Vikas   Gupta            viki123    9911224411  10-DEC-89
+Abhinav	Shukla	abhi654	abhparrot	9977445671	12-OCT-99
+Aly	Mirza	aly@675	ally	8877445671	20-DEC-17
+*/
+
+Desc Customer ;
+
+Delete from CUSTOMER
+where first_name = 'Vikas' ;
 
 /*
-Column	Null?	Type
-THEATRE_NAME	 - 	VARCHAR2(20)
-NO_OF_SEATS	 - 	NUMBER(3,0)
-TICKET_PRICE	 - 	NUMBER(5,2)
+OUTPUT:
+
+After Delete Command
+
+FIRST_NAME	LAST_NAME	USERNAME	PASSWORD	MOBILE_NUMBER	DATE_OF_BIRTH
+Niki	Tamboli	Nikki123	ben23	8877445671	12-NOV-97
+Abhinav	Shukla	abhi654	abhparrot	9977445671	12-OCT-99
+Aly	Mirza	aly@675	ally	8877445671	20-DEC-17
 */
