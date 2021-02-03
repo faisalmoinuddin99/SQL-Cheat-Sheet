9c5d56ceee4674015cb0a326224fb22a34181c03
faisal <faisal25march99@gmail.com>
2021-02-03 11:18:20 +0530

    ADD ticket_price column in Movie_Theatre table

--- HandsOnExercise.sql
+++ HandsOnExercise.sql
@@ -1,37 +1,15 @@
-/*
-We can also apply ALTER at TABLE level
+-- ADD ticket_price column in Movie_Theatre table
 
-1. MODIFY a table name
+ALTER TABLE MOVIE_THEATRE
+ADD ticket_price Number(3);
 
-The Syntax  for this operation is as follows:
-
-ALTER TABLE table_name
-RENAME TO new_table_name ;T
-*/
-
--- Renaming a table name
-
-CREATE TABLE Theatre(
-theatre_name VARCHAR2(20),
-no_of_seats NUMBER(3)
-);
-
-DESCRIBE Theatre ;
+DESCRIBE MOVIE_THEATRE
 
 /*
-Column      	Null?	Type
-THEATRE_NAME	 -  	VARCHAR2(20)
-NO_OF_SEATS	 - 	        NUMBER(3,0)
+Column	Null?	Type
+THEATRE_NAME	 - 	VARCHAR2(20)
+NO_OF_SEATS	 - 	NUMBER(3,0)
+TICKET_PRICE	 - 	NUMBER(3,0)
 */
 
-ALTER TABLE Theatre
-RENAME TO movie_Theatre ;
-
-DESCRIBE Theatre ; -- ERROR Theatre Table not exisit
-DESCRIBE MOVIE_THEATRE 
-/*
-Column      	Null?	Type
-THEATRE_NAME	 -  	VARCHAR2(20)
-NO_OF_SEATS	 - 	        NUMBER(3,0)
-*/
 
